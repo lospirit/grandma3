3c053a7325941025456aaf84e0bc9b593642e842
lospirit <56344337+lospirit@users.noreply.github.com>
2025-08-27 13:10:56 +0300

    Update pixel_picker.lua

--- pixel_picker.lua
+++ pixel_picker.lua
@@ -1,8 +1,8 @@
 --[[
 [:::::::::::::::::::::::::::::::::::::::::::]
- | Program >> PixelPicker GrandMA 3
- | Version >> 0.0.1	
- | Updated >> 26.08.2025	
+ | Program >> PixelPicker GrandMA 3       
+ | Version >> 0.0.2	
+ | Updated >> 27.08.2025	
  | Modification for GrandMa 3  >> lospirit	
  | Original Author  >> AlexSaltsev	 
 [:::::::::::::::::::::::::::::::::::::::::::]
@@ -12,17 +12,30 @@ return function()
 
     local groupNumber = tonumber(TextInput('Enter Group', '1'))
     local pixelStart  = tonumber(TextInput('Enter 1st pixel', '1'))
-    local pixelEnd    = tonumber(TextInput('Enter last pixel', '20'))
+    local pixelEnd    = tonumber(TextInput('Enter last pixel or 0 if you want to select sub group', '20'))
+
+    if not groupNumber or not pixelStart or pixelEnd == nil then
+        Printf("Invalid input")
+        return
+    end
 
     Cmd('ClearAll')
     Cmd('Group ' .. groupNumber)
 
     local cmdline = ''
-    for i = pixelStart, pixelEnd do
-        cmdline = cmdline .. ' + If Fixture *.' .. i
+ 
+    if pixelEnd == 0 then
+        -- select only one sub group in fixture 
+        cmdLine = " If Fixture *." .. pixelStart
+        Cmd(cmdLine)
+        Cmd("Down")
+    else
+        -- pixel range
+        for i = pixelStart, pixelEnd do
+            cmdLine = cmdLine .. " + If Fixture *." .. i
+        end
+        Cmd(cmdLine)
     end
-
-    Cmd(cmdline)
 
 end
 
